ca5d2e206f8c84758e35cc3f0554166ddf6ff692
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-01-23 17:47:50 +0530

    styled the error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,17 +2,29 @@ import ErrorImage from "../images/404_Error.jpg";
 import { useRouteError, Link } from "react-router-dom"; // import useRouteError for routing error
 
 const Error = () => {
-  // call useRouteError so we can access error data while routing
+  // used useRouteError as to access error data while routing
   const err = useRouteError();
+  console.log(err);
   return (
-    <div className="">
-      <img src={ErrorImage} alt="Error" />
-      <h1>Oops! The restaurant you're looking for can't be found.</h1>
-      <h3 className="">{err.data}</h3>
-      <h3 className="">
-        <Link to="/">Back Home</Link>
-      </h3>
-    </div>
+    <>
+      <div className="absolute size-full">
+        <img
+          src={ErrorImage}
+          alt="Error"
+          className="size-full -z-10 opacity-40"
+        />
+      </div>
+      <div className="absolute text-center ml-[25%]">
+        <h1 className=" font-semibold mt-10 text-2xl">
+          Oops! The restaurant you're looking for can't be found.
+        </h1>
+        <h3 className="text-lg mt-2">Error Code: {err.status}</h3>
+        <h3 className="text-lg mt-2">{err.data}</h3>
+        <button className="bg-teal-400 rounded mt-2 w-24 h-8">
+          <Link to="/">Back Home</Link>
+        </button>
+      </div>
+    </>
   );
 };
 

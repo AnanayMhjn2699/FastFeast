520a08b66315a7a59477a78bab65d2ba30e914df
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-01-23 13:53:43 +0530

    styled the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,19 +2,17 @@ import food from "../images/burger-image.png";
 
 const About = () => {
   return (
-    <div className="">
-      <div className="flex justify-center">
-        <h1 className="font-bold">
-          Welcome to The world of <span>Tasty & Fresh Food</span>
+    <div className="flex">
+      <img src={food} alt="Burger's" className="size-auto mt-10 ml-5" />
+      <div className="justify-center ml-18 mt-10">
+        <h1 className="font-bold text-3xl">
+          Welcome to the world of <span>Tasty & Fresh Food</span>
           <br />
         </h1>
-        <h4>
-          "Better you will feel if you eat a Fast<span>Feast</span> healthy
-          meal"
+        <h4 className="text-center mt-3 text-lg">
+          "Better you will feel if you eat a Fast
+          <span className="text-red-600">Feast</span>'s healthy meal"
         </h4>
-      </div>
-      <div className="">
-        <img src={food} alt="Burger's " />
       </div>
     </div>
   );

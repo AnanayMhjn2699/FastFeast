2c023a859a02cc5f186748755d763d5f23248210
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-01-24 12:03:56 +0530

    unit tested the contact us page

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -1,5 +1,5 @@
 import Contact from "../Contact";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 test("should load the contact component", () => {
@@ -8,4 +8,30 @@ test("should load the contact component", () => {
   const heading = screen.getByRole("heading");
 
   expect(heading).toBeInTheDocument();
+});
+
+test("should have 3 input fields", () => {
+  render(<Contact />);
+
+  const inputFields = screen.getAllByRole("textbox");
+
+  expect(inputFields.length).toBe(3);
+});
+
+test("should have submit button", () => {
+  render(<Contact />);
+
+  const button = screen.getAllByRole("button");
+
+  expect(button.length).toBe(1);
+});
+
+test("should print message after submitting", () => {
+  render(<Contact />);
+
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
+
+  expect("Thank you for contacting FastFeast, We will reply ASAP.")
+    .toBeInTheDocument;
 });

d6c08ce11b65c0685854d8832ccdd810bc4ae026
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-03-13 22:26:14 +0530

    updated the code

--- src/components/Shimmer2.jsx
+++ src/components/Shimmer2.jsx
@@ -12,7 +12,7 @@ const Shimmer2 = () => {
         <div role="status">
           <svg
             aria-hidden="true"
-            class="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -26,7 +26,7 @@ const Shimmer2 = () => {
               fill="currentFill"
             />
           </svg>
-          <span class="sr-only">Loading...</span>
+          <span className="sr-only">Loading...</span>
         </div>
       </div>
     </div>

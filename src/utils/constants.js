be0dcaa398fd01b73e989cc6eee2ef01bc611032
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-05-14 12:43:30 +0530

    updated the CDN Url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
+// export const CDN_URL =
+//   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const SWIGGY_API_URL =
   "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5996207&lng=77.3736349&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";

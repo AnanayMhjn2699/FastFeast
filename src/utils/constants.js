e6d028ec6f9437f0da940943881f7e19757030d2
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-05-14 12:48:59 +0530

    updated the constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 // export const CDN_URL =
 //   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+// https://thingproxy.freeboard.io/  :::add this before any url to overcome the CORS issue
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
